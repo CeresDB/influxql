36fc4d873e7d9b544560e465cfb2224883bddf98
Jiacai Liu <dev@liujiacai.net>
2023-04-03 20:28:36 +0800

    add ceresdb hack, convert arrow schema to influxql (#1)
    * add ceresdb hack, convert arrow schema to influxql
    
    * fix CI
    
    * add hack file

---
+++ iox_query_influxql/src/plan/hack.rs
@@ -0,0 +1,76 @@
+use std::{collections::HashMap, sync::Arc};
+
+use arrow::datatypes::{DataType, Field, Schema};
+use datafusion::common::{DataFusionError, Result};
+
+/// Convert arrow schema from CeresDB to format used in InfluxDB
+pub fn ceresdb_schema_to_influxdb(arrow_schema: Arc<Schema>) -> Result<Arc<Schema>> {
+    let md = arrow_schema.metadata();
+    let time_idx = md
+        .get("schema::timestamp_index")
+        .ok_or(DataFusionError::External(
+            "no timestamp index".to_string().into(),
+        ))?;
+    let time_idx: usize = time_idx.parse().map_err(|e| {
+        DataFusionError::External(format!("timestamp index value isn't usize, err:{e:?}").into())
+    })?;
+
+    let fields = arrow_schema
+        .fields
+        .iter()
+        .enumerate()
+        .map(|(i, f)| {
+            let is_tag = if let Some(v) = f.metadata().get("field::is_tag") {
+                v == "true"
+            } else {
+                false
+            };
+
+            let data_type = f.data_type();
+            let influxql_col_type = if i == time_idx {
+                "iox::column_type::timestamp"
+            } else if is_tag {
+                "iox::column_type::tag"
+            } else {
+                match data_type {
+                    DataType::Float64 | DataType::Float32 | DataType::Float16 => {
+                        "iox::column_type::field::float"
+                    }
+                    DataType::Int8 | DataType::Int16 | DataType::Int32 | DataType::Int64 => {
+                        "iox::column_type::field::integer"
+                    }
+                    DataType::UInt8 | DataType::UInt16 | DataType::UInt32 | DataType::UInt64 => {
+                        "iox::column_type::field::uinteger"
+                    }
+                    DataType::Utf8 | DataType::LargeUtf8 => "iox::column_type::field::string",
+                    DataType::Boolean => "iox::column_type::field::boolean",
+                    _ => {
+                        return Err(DataFusionError::Internal(format!(
+                            "influxql don't supported this datatype, type:{data_type}"
+                        )));
+                    }
+                }
+            };
+
+            let data_type = if i == time_idx {
+                // this is required by iox, time is ms unit in ceresdb
+                schema::TIME_DATA_TYPE()
+            } else {
+                data_type.clone()
+            };
+            // tsid column is treated as field, so it's nullable
+            let nullable = if "tsid" == f.name() {
+                true
+            } else {
+                f.is_nullable()
+            };
+            let md = HashMap::from([(
+                "iox::column::type".to_string(),
+                influxql_col_type.to_string(),
+            )]);
+            Ok(Field::new(f.name(), data_type, nullable).with_metadata(md))
+        })
+        .collect::<Result<_>>()?;
+
+    Ok(Arc::new(Schema::new_with_metadata(fields, md.clone())))
+}
--- iox_query_influxql/src/plan/mod.rs
+++ iox_query_influxql/src/plan/mod.rs
@@ -1,6 +1,7 @@
 mod expr_type_evaluator;
 mod field;
 mod field_mapper;
+mod hack;
 mod planner;
 mod planner_rewrite_expression;
 mod planner_time_range_expression;
@@ -11,6 +12,7 @@ mod util;
 mod util_copy;
 mod var_ref;
 
+pub use hack::ceresdb_schema_to_influxdb;
 pub use planner::InfluxQLToLogicalPlan;
 pub use planner::SchemaProvider;
 pub(crate) use util::parse_regex;
