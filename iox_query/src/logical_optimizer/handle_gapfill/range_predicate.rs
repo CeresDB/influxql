b77565fd7af7e89af8c1579cfe60fc856e66d395
tanruixiang <tanruixiang0104@gmail.com>
2023-07-10 17:05:44 +0800

    fix: support get range from and expr

--- iox_query/src/logical_optimizer/handle_gapfill/range_predicate.rs
+++ iox_query/src/logical_optimizer/handle_gapfill/range_predicate.rs
@@ -139,16 +139,116 @@ impl TimeRange {
     }
 
     fn with_lower(self, start: Bound<Expr>) -> Self {
+        let max_start ;
+        match (start, self.0.start) {
+            (Bound::Included(x), Bound::Included(y)) => {
+                max_start = match x > y {
+                    true => {
+                        Bound::Included(x)
+                    },
+                    false => Bound::Included(y),
+                };
+
+            }
+            (Bound::Included(x), Bound::Excluded(y)) => {
+                max_start = match x > y {
+                    true => {
+                        Bound::Included(x)
+                    },
+                    false => Bound::Excluded(y),
+                };
+            }
+            (Bound::Excluded(x), Bound::Included(y)) => {
+                max_start = match x >= y {
+                    true => {
+                        Bound::Excluded(x)
+                    },
+                    false => Bound::Included(y),
+                };
+            }
+            (Bound::Excluded(x), Bound::Excluded(y)) => {
+                max_start = match x > y {
+                    true => {
+                        Bound::Excluded(x)
+                    },
+                    false => Bound::Excluded(y),
+                };
+            }
+            (Bound::Unbounded, y) =>{
+                max_start = match  y {
+                        Bound::Included(x) => Bound::Included(x),
+                        Bound::Excluded(x) => Bound::Excluded(x),
+                        Bound::Unbounded => Bound::Unbounded,
+                }
+            }
+            (x, Bound::Unbounded) =>{
+                max_start = match  x {
+                        Bound::Included(x) => Bound::Included(x),
+                        Bound::Excluded(x) => Bound::Excluded(x),
+                        Bound::Unbounded => Bound::Unbounded,
+                }
+            }
+        }
         Self(Range {
-            start,
+            start: max_start,
             end: self.0.end,
         })
     }
 
     fn with_upper(self, end: Bound<Expr>) -> Self {
+        let min_end ;
+        match (end, self.0.end) {
+            (Bound::Included(x), Bound::Included(y)) => {
+                min_end = match x < y {
+                    true => {
+                        Bound::Included(x)
+                    },
+                    false => Bound::Included(y),
+                };
+
+            }
+            (Bound::Included(x), Bound::Excluded(y)) => {
+                min_end = match x < y {
+                    true => {
+                        Bound::Included(x)
+                    },
+                    false => Bound::Excluded(y),
+                };
+            }
+            (Bound::Excluded(x), Bound::Included(y)) => {
+                min_end = match x <= y {
+                    true => {
+                        Bound::Excluded(x)
+                    },
+                    false => Bound::Included(y),
+                };
+            }
+            (Bound::Excluded(x), Bound::Excluded(y)) => {
+                min_end = match x < y {
+                    true => {
+                        Bound::Excluded(x)
+                    },
+                    false => Bound::Excluded(y),
+                };
+            }
+            (Bound::Unbounded, y) =>{
+                min_end = match  y {
+                        Bound::Included(x) => Bound::Included(x),
+                        Bound::Excluded(x) => Bound::Excluded(x),
+                        Bound::Unbounded => Bound::Unbounded,
+                }
+            }
+            (x, Bound::Unbounded) =>{
+                min_end = match  x {
+                        Bound::Included(x) => Bound::Included(x),
+                        Bound::Excluded(x) => Bound::Excluded(x),
+                        Bound::Unbounded => Bound::Unbounded,
+                }
+            }
+        }
         Self(Range {
             start: self.0.start,
-            end,
+            end: min_end,
         })
     }
 }
@@ -296,6 +396,46 @@ mod tests {
                 Range {
                     start: Bound::Included(lit_timestamp_nano(1000)),
                     end: Bound::Excluded(lit_timestamp_nano(2000)),
+                },
+            ),
+            (
+                "lt_and_less",
+                col("time")
+                    .lt(lit_timestamp_nano(1000))
+                    .and(col("time").lt(lit_timestamp_nano(500))),
+                Range {
+                    start: Bound::Unbounded,
+                    end: Bound::Excluded(lit_timestamp_nano(500)),
+                },
+            ),
+            (
+                "lt_and_greater",
+                col("time")
+                    .lt(lit_timestamp_nano(500))
+                    .and(col("time").lt(lit_timestamp_nano(1000))),
+                Range {
+                    start: Bound::Unbounded,
+                    end: Bound::Excluded(lit_timestamp_nano(500)),
+                },
+            ),
+            (
+                "gt_and_greater",
+                col("time")
+                    .gt(lit_timestamp_nano(500))
+                    .and(col("time").gt(lit_timestamp_nano(1000))),
+                Range {
+                    start: Bound::Excluded(lit_timestamp_nano(1000)),
+                    end: Bound::Unbounded,
+                },
+            ),
+            (
+                "gt_and",
+                col("time")
+                    .gt(lit_timestamp_nano(1000))
+                    .and(col("time").gt(lit_timestamp_nano(500))),
+                Range {
+                    start: Bound::Excluded(lit_timestamp_nano(1000)),
+                    end: Bound::Unbounded,
                 },
             ),
             (
